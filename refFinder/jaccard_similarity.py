54b15c3da431c2c7403dd903533985db2b1234e9
licjon <67080615+licjon@users.noreply.github.com>
2022-03-04 09:08:18 -0500

    Delete jaccard_similarity.py

--- refFinder/jaccard_similarity.py
+++
@@ -1,7 +0,0 @@
-def jaccard_similarity(x, y):
-  """ Takes 2 lists of string (sentences) and
-      returns the jaccard similarity between two lists (float).
-  """
-  intersection_cardinality = len(set.intersection(*[set(x), set(y)]))
-  union_cardinality = len(set.union(*[set(x), set(y)]))
-  return intersection_cardinality / float(union_cardinality)
