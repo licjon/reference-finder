e134f99b372988bd3bc8c11ff303a40d0ab7f834
licjon <67080615+licjon@users.noreply.github.com>
2022-03-04 09:09:02 -0500

    Delete num_search.py

--- refFinder/num_search.py
+++
@@ -1,9 +0,0 @@
-sentence = ['there', 'are', '10', 'cats', 'in', '10.1', 'houses', '.']
-
-def find_num(sentence):
-    for grapheme in sentence:
-        try:
-            if float(grapheme):
-                print(grapheme)
-        except ValueError:
-            pass
